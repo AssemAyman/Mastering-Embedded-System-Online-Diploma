3b412a8a841eda09d905befc9cf089d240ba11a8
Assem Ayman <assemayman79@gmail.com>
2022-07-09 01:56:18 +0200

    solution of ex2

---
+++ Assignments/C_Basics/Assignment1/ex2.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : ex2.c
+ Author      : Assem Ayman
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	int x;
+
+	printf("Enter a integer: ");
+	fflush(stdout); fflush(stdin);
+	scanf("%d",&x);
+	printf("You entered: %d",x);
+
+	return 0;
+}
