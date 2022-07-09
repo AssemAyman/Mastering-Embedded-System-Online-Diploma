26ffa075e76ce50ba53be6f21e407689fc12488b
Assem Ayman <assemayman79@gmail.com>
2022-07-09 03:11:38 +0200

    solution of ex4

---
+++ Assignments/C_Basics/Assignment1/ex4.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : ex4.c
+ Author      : Assem Ayman
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+	float x,y;
+	printf("Enter two numbers: ");
+	fflush(stdout); fflush(stdin);
+	scanf("%f %f",&x,&y);
+	fflush(stdout); fflush(stdin);
+	printf("Product: %f",x*y);
+	return 0;
+}
