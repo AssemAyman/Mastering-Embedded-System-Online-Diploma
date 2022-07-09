7d61b43ad1fbcdc8fd5177c9c4f205e2c71b34fd
Assem Ayman <assemayman79@gmail.com>
2022-07-09 23:13:53 +0200

    solution of ex4

---
+++ Assignments/C_Basics/Assignment2/ex4.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : ex4.c
+ Author      : Assem Ayman
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+	float x;
+	printf("Enter a number: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%f",&x);
+	if(x>0)
+		printf("%0.2f is positive",x);
+	else if(x<0)
+		printf("%0.2f is negative",x);
+	else
+		printf("You entered Zero");
+	return 0;
+}
