8c876fa245f19a57220ac8f53cebe80f57671012
Assem Ayman <assemayman79@gmail.com>
2022-07-09 23:00:46 +0200

    solution of ex3

---
+++ Assignments/C_Basics/Assignment2/ex3.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : ex3.c
+ Author      : Assem Ayman
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+	float x,y,z;
+	printf("Enter three numbers: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%f %f %f",&x,&y,&z);
+	if(x>y && x>z)
+		printf("Largest number = %f",x);
+	else if(y>z)
+		printf("Largest number = %f",y);
+	else
+		printf("Largest number = %f",z);
+	return 0;
+}
