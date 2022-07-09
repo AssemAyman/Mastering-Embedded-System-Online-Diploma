e66464334034d67b7ab7c2261052193435741326
Assem Ayman <107751300+AssemAyman@users.noreply.github.com>
2022-07-09 23:17:29 +0200

    Update ex4.c

--- Assignments/C_Basics/Assignment1/ex4.c
+++ Assignments/C_Basics/Assignment1/ex4.c
@@ -4,7 +4,7 @@
  Author      : Assem Ayman
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : print product of two numbers in C, Ansi-style
  ============================================================================
  */
 
