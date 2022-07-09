2f7b25d607f17874c070e376479463344f51f293
Assem Ayman <107751300+AssemAyman@users.noreply.github.com>
2022-07-09 23:19:32 +0200

    Update ex5.c

--- Assignments/C_Basics/Assignment1/ex5.c
+++ Assignments/C_Basics/Assignment1/ex5.c
@@ -4,7 +4,7 @@
  Author      : Assem Ayman
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : print ASCII value of a character in C, Ansi-style
  ============================================================================
  */
 
