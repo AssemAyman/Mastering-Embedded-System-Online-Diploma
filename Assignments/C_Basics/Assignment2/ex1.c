78dd077e2585e7363c30da18ca96c2e4ea718cbb
Assem Ayman <assemayman79@gmail.com>
2022-07-09 22:18:42 +0200

    solution of ex1

---
+++ Assignments/C_Basics/Assignment2/ex1.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : ex1.c
+ Author      : Assem Ayman
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+
+int main(void) {
+
+	int x;
+	printf("Enter an integer you want to check: ");
+	fflush(stdout); fflush(stdin);
+	scanf("%d",&x);
+	x%2 == 0? printf("%d is even",x) : printf("%d is odd",x);
+	return 0;
+}
