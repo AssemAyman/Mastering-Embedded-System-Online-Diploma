86cf1c7726166df187015d26f3aac0ec7eb6a9fd
Assem Ayman <107751300+AssemAyman@users.noreply.github.com>
2022-08-01 18:44:00 +0200

    Update ex3.c

--- Assignments/C_Assignments/4_C_Struct&Union&enum/ex3.c
+++ Assignments/C_Assignments/4_C_Struct&Union&enum/ex3.c
@@ -25,19 +25,24 @@ int main(void) {
 	num2 = get_Data(second);
 	struct complex SUM = add(num1,num2);
 	printf("Sum = %0.2f + %0.2fi",SUM.real,SUM.imaginary);
+
 	return 0;
 }
 struct complex get_Data(enum index n){
+
 	struct complex temp;
 	printf("For %dst complex number\n",n);
 	printf("Enter real imaginary respectively: ");
 	fflush(stdin); fflush(stdout);
 	scanf("%f %f",&temp.real,&temp.imaginary);
+
 	return temp;
 }
 struct complex add(struct complex num1,struct complex num2){
+
 	struct complex sum;
 	sum.real = num1.real + num2.real;
 	sum.imaginary = num1.imaginary + num2.imaginary;
+
 	return sum;
 }
