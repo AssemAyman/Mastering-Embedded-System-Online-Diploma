2f47a9d7094e0e4fef08114a92187aa58f28a984
Assem Ayman <assemayman79@gmail.com>
2023-07-30 08:32:05 +0300

    add

---
+++ Assignments/5_MCU_Essential_Peripherals/Section2/main.c
@@ -0,0 +1,32 @@
+/*
+ * Section2.c
+ *
+ * Created: 7/30/2023 2:08:19 AM
+ * Author : assem
+ */ 
+
+#include "LCD/LCD.h"
+#include "Key_Pad/Key_Pad.h"
+
+int main(void)
+{
+	char Pressed_Key;
+    LCD_Init();
+	KEYPAD_INIT();
+	LCD_Send_A_String("HI I am Eng/Assem,I live in Egypt and I am 21 years old.");
+    while (1) 
+    {
+		Pressed_Key = KEYPAD_GET_KEY();
+		switch(Pressed_Key){
+			//No pressed Key
+			case 'A': break;
+			//ON/C is pressed
+			case '?': 
+			LCD_Clear_Screen();
+			break;
+			//Others
+			default:
+			LCD_Send_A_Character(Pressed_Key);
+       }
+    }
+}
