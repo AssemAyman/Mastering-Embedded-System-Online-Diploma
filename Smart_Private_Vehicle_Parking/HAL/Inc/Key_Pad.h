10704b52be9ad65ff57047e1bfe69006d9c5bfa6
Assem Ayman <107751300+AssemAyman@users.noreply.github.com>
2023-10-05 02:50:43 +0300

    Add files via upload

---
+++ Smart_Private_Vehicle_Parking/HAL/Inc/Key_Pad.h
@@ -0,0 +1,27 @@
+/*
+ * Key_Pad.h
+ *
+ * Created: 7/30/2023 4:51:30 AM
+ *  Author: assem
+ */
+
+#ifndef KEY_PAD_H_
+#define KEY_PAD_H_
+
+#include "GPIO.h"
+/*=========Configurations============*/
+#define KeyPad_PORT		GPIOB
+/*===================================*/
+#define R0 GPIO_PIN_0
+#define R1 GPIO_PIN_1
+#define R2 GPIO_PIN_3
+#define R3 GPIO_PIN_4
+#define C0 GPIO_PIN_5
+#define C1 GPIO_PIN_6
+#define C2 GPIO_PIN_7
+
+extern void KEYPAD_INIT(void);
+extern char KEYPAD_GET_KEY(void);
+
+#endif /* KEY_PAD_H_ */
+
---
+++ Smart_Private_Vehicle_Parking/HAL/Inc/LED.h
@@ -0,0 +1,19 @@
+/*
+ * LED.h
+ *
+ *  Created on: Oct 4, 2023
+ *      Author: assem
+ */
+
+#ifndef LED_H_
+#define LED_H_
+
+#include "GPIO.h"
+#include "Timer.h"
+
+extern void LED_init();
+
+extern void RED_LED_ON();
+extern void Green_LED_ON();
+
+#endif /* LED_H_ */
