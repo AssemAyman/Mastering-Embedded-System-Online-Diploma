d172ce955aaf2b80d9005f6d8f6545db4f75fe53
Assem Ayman <107751300+AssemAyman@users.noreply.github.com>
2024-10-18 01:54:44 +0300

    Update GPS.h

--- STM32F103C6_Drivers/HAL/GPS/GPS.h
+++ STM32F103C6_Drivers/HAL/GPS/GPS.h
@@ -11,10 +11,13 @@
 #include "USART.h"
 #include <string.h>
 
-#define USARTx 			USART1
+/*======== Settings ========*/
+#define USARTx 			USART1    // Choose between USART1 , USART2 , USART3
+/*==========================*/
 
 uint8_t location_data[40],done;
 
+/*=================== APIs ========================*/
 extern void GPS_Init();
 extern void Get_Location(float*, float*);
 
